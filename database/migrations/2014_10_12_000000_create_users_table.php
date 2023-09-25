e4e39e81c30112fc91212b05020b332d538d0239
Pascual Eburi <pascualeburi@gmail.com>
2023-09-25 07:15:47 +0200

    feat: Define the structure for the [users] table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->enum('status', ['active', 'block', 'inactive'])->default('inactive');
+            $table->enum('role', ['admin', 'seller', 'client'])->default('client');
+            $table->text('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
