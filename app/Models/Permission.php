3c2069d177164d97c79bf933917add49a29b385c
Pascual Eburi <pascualeburi@gmail.com>
2023-10-01 23:47:41 +0200

    Define many-to-many relationship in Permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Permission extends Model
 {
@@ -11,4 +12,13 @@ class Permission extends Model
     protected $fillable = [
         'name'
     ];
+
+    /**
+     *  Get the groups thst has the permission
+     * @return BelongsToMany
+     */
+    public function groups(): BelongsToMany
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
