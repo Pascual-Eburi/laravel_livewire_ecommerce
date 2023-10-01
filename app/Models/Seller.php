b1deb0cfa1e2582926ced4dd5edc34fdfe2dc680
Pascual Eburi <pascualeburi@gmail.com>
2023-10-01 19:33:13 +0200

    Make relationship with user model

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Seller extends Model
 {
@@ -19,4 +20,14 @@ class Seller extends Model
         "payment_method",
         "payment_email"
     ];
+
+
+    /**
+     * Get the user associated whit the seller.
+     */
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
