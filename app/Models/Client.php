a4078159f9c9da569838131031218e84dc2b5fa8
Pascual Eburi <pascualeburi@gmail.com>
2023-10-01 19:31:44 +0200

    Make relationship with user model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Client extends Model
 {
@@ -17,4 +18,12 @@ class Client extends Model
     protected $fillable = [
         'address',
     ];
+
+    /**
+     * Get the user associated with the client
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
