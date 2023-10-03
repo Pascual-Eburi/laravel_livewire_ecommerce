2fee411b3fa1a687c1d199199891a795869f93f5
Pascual Eburi <pascualeburi@gmail.com>
2023-10-03 22:28:11 +0200

    Implements logout method & functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -13,21 +15,49 @@ class UserController extends Controller
         'password' =>'required|min:5|max:45',
     ];
 
-    public  function  login (Request $request){
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public  function  login (Request $request): RedirectResponse
+    {
         // check if user login with email or username
         $login_id = filter_var($request->login_id, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
-
 
         // validate data
         $request->validate([
             'login_id' => $this->rules[$login_id],
             'password' => $this->rules['password'],
+        ], [
+            'login_id.required' => "Email or Username is required",
+            'login_id.email' => "Invalid Email address",
+            'login_id.exists' => $login_id === 'email' ? 'Wrong email' : 'Wrong Username',
+            'password.required' => "Password is required"
         ]);
 
+        //
+        $logid_data = [
+            $login_id => $request->login_id, // login_id => email | username
+            'password' => $request->password
+        ];
+
+        if ( Auth::attempt($logid_data)){
+            return redirect()->route('home');
+        }
+
+        session()->flash('fail', 'Incorrect credentials');
+        return redirect()->route('login');
     }
 
 
-    public function logout(Request $request){
-
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+        session()->flash('fail', 'Your are logged out');
+        return redirect()->route('auth.login');
     }
 }
