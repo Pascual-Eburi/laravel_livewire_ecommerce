ebcd29109a957e2a205ba5fbf78d995f3b862deb
Pascual Eburi <pascualeburi@gmail.com>
2023-10-03 22:31:21 +0200

    Add logout form to header component

--- resources/views/components/backend/navigation/header.blade.php
+++ resources/views/components/backend/navigation/header.blade.php
@@ -155,37 +155,44 @@
                 </div>
             </div>
         </div>
-        <div class="user-info-dropdown">
-            <div class="dropdown">
-                <a
-                    class="dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-toggle="dropdown"
-                >
+        @if( Auth::check())
+            <div class="user-info-dropdown">
+                <div class="dropdown">
+                    <a
+                        class="dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-toggle="dropdown"
+                    >
 							<span class="user-icon">
 								<img src="/backend/vendors/images/photo1.jpg" alt="" />
 							</span>
-                    <span class="user-name">Ross C. Lopez</span>
-                </a>
-                <div
-                    class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
-                >
-                    <a class="dropdown-item" href="profile.html"
-                    ><i class="dw dw-user1"></i> Profile</a
+                        <span class="user-name">Ross C. Lopez</span>
+                    </a>
+                    <div
+                        class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
                     >
-                    <a class="dropdown-item" href="profile.html"
-                    ><i class="dw dw-settings2"></i> Setting</a
-                    >
-                    <a class="dropdown-item" href="faq.html"
-                    ><i class="dw dw-help"></i> Help</a
-                    >
-                    <a class="dropdown-item" href="login.html"
-                    ><i class="dw dw-logout"></i> Log Out</a
-                    >
+                        <a class="dropdown-item" href="profile.html"
+                        ><i class="dw dw-user1"></i> Profile</a
+                        >
+                        <a class="dropdown-item" href="profile.html"
+                        ><i class="dw dw-settings2"></i> Setting</a
+                        >
+                        <a class="dropdown-item" href="faq.html"
+                        ><i class="dw dw-help"></i> Help</a
+                        >
+                        <a class="dropdown-item" href="{{ route('auth.logoutHandler') }}"
+                           onclick="event.preventDefault();document.getElementById('logoutForm').submit();"
+                        ><i class="dw dw-logout"></i> Log Out</a
+                        >
+                        <form id="logoutForm" method="POST" action="{{ route('auth.logoutHandler') }}">
+                            @csrf
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endif
+
         <div class="github-link">
             <a href="https://github.com/dropways/deskapp" target="_blank"
             ><img src="/backend/vendors/images/github.svg" alt=""
