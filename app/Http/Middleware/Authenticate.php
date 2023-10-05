96ed3220225c448a125bbe5c3390decb9a3283ca
Pascual Eburi <pascualeburi@gmail.com>
2023-10-05 04:47:38 +0200

    Change auth redirection route

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : '/auth/login'; //route('login');
+        return $request->expectsJson() ? null : route('auth.login');
     }
 }
