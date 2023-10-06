e8869103222b01c5db023190d880f72d149abb6c
Pascual Eburi <pascualeburi@gmail.com>
2023-10-06 15:59:20 +0200

    Add view for alert component

---
+++ resources/views/components/backend/notification/alert.blade.php
@@ -0,0 +1,8 @@
+
+<!-- Render a alert component -->
+<div class="alert alert-{{ $type }}">
+    {{ $message }}
+    <button class="close" type="button" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
